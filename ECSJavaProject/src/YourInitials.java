d85af3c854faf6579df4c9ff22e4f5f21fdd9a9c
drewdwitte <WitteD7791@sduhsd.lan>
2017-11-30 09:55:51 -0800

    My Initials

---
+++ ECSJavaProject/src/YourInitials.java
@@ -0,0 +1,15 @@
+public class YourInitials
+{
+	public static void main(String[] args)
+	{
+		System.out.println("DDDD    EEEEE   W     W ");
+		System.out.println("D   D   E       W     W");
+		System.out.println("D   D   E       W     W");
+		System.out.println("D   D   EEE     W  W  W");
+		System.out.println("D   D   E       WW   WW");
+		System.out.println("D   D   E       WW   WW");
+		System.out.println("DDDD    EEEEE   W     W");
+		
+	
+	}
+}
