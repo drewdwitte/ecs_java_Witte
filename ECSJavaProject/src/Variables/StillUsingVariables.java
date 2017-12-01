b5ef0585c3c7bcf1dba5002dd636b182c9374be3
drewdwitte <WitteD7791@sduhsd.lan>
2017-12-01 10:14:10 -0800

    can't stop the variables

---
+++ ECSJavaProject/src/Variables/StillUsingVariables.java
@@ -0,0 +1,17 @@
+package Variables;
+
+public class StillUsingVariables 
+{
+
+	public static void main(String[] args)
+	{
+		int yourgradyear;
+		String yourname;
+		
+		yourgradyear=2019;
+		yourname="Drew Witte";
+		
+		System.out.println("My name is "+yourname+" and I'll graduate in "+yourgradyear+".");
+	}
+	
+}
