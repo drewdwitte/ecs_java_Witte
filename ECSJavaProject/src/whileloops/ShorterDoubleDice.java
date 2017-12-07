dcba8a16220d14af7a87a69da9708c8eb9cd6882
drewdwitte <WitteD7791@sduhsd.lan>
2017-12-07 11:07:54 -0800

    shortdubsdie

--- ECSJavaProject/src/whileloops/ShorterDoubleDice.java
+++ ECSJavaProject/src/whileloops/ShorterDoubleDice.java
@@ -1,7 +1,35 @@
 package whileloops;
 
+import java.util.Random;
+
 public class ShorterDoubleDice 
 {
+	public static void main(String[] args)
+	{
+		Random r = new Random();
+		 
+		int x = 1 + r.nextInt(6),y=1+r.nextInt(6);
+		
+		
+		System.out.println("HERE COMES THE DICE!!");
+		
+		System.out.println("Roll #1: "+x);
+		System.out.println("Roll #2: "+y);
+		System.out.println("The total is "+(x+y));
+	
+	do
+	{
+		 x = 1 + r.nextInt(6);
+		 y=1+r.nextInt(6);
+		 
+		System.out.println("HERE COMES THE DICE!!");
+		
+		System.out.println("Roll #1: "+x);
+		System.out.println("Roll #2: "+y);
+		System.out.println("The total is "+(x+y)); 
+	}while(x!=y);
+	
+	}
+}
 
 	
-}
